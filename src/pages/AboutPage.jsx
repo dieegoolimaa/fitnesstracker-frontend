eab7b23f9d4e709a63f021d70af9e7d8893a5f48
kingblocks <kivanckeskinbora@gmail.com>
2024-05-01 12:30:30 +0200

    Editted text and updated links, not done yet

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -32,42 +32,35 @@ const AboutPage = () => {
     <div className={styles.aboutPage}>
       <h1>About Us</h1>
       <p>
-        Welcome to Poketeam—your ultimate Pokémon companion! Discover, compare, and create your
-        dream team with ease on our intuitive platform. Dive into our extensive database to explore
-        detailed stats on all Pokémon, from classics to legendaries. Whether you're a seasoned
-        trainer or just starting your journey, Poketeam equips you with the tools to craft your
-        perfect lineup. Join our vibrant community, share strategies, and embark on endless
-        adventures. Start building your ultimate team today and unleash the power of your favorite
-        Pokémon with Poketeam!
+      Welcome to our Fitness tracker—your ultimate fitness companion! Track, manage, and optimize your fitness journey with ease on our intuitive platform. Dive into our online exercise database to explore detailed descriptions and instructions on a wide range of workouts, from bodyweight to weighted strength training. Whether you're a seasoned athlete or just starting your fitness journey, Fitness tracker equips you with the tools to set and achieve your goals. Join our vibrant community, share workout tips, and embark on endless fitness adventures. Start building your personalized training program today and unleash the power of your potential with fitness tracker! 🏋️‍♂️💪
       </p>
 
       <h2>Team Members</h2>
       <p>
-        We are a team of developers who love to code. Ironhack is our bootcamp. We are based in
-        Germany. We hope to see you in other projects.
+        We are a team of developers who love to code. Ironhack is our bootcamp. We are a small group of 3. We hope to see you in other projects.
       </p>
 
       <div className={styles.teamMembers}>
         <TeamMember
           name='Rui Folgado'
           role='Developer'
-          github='https://github.com/'
+          github='https://github.com/Folgad0'
           linkedin='https://www.linkedin.com/in/rui-folgado/'
           imageUrl={rui}
         />
 
         <TeamMember
-          name='Diego Lima'
+          name='Diego Fernandes'
           role='Developer'
-          github='https://github.com/kingblocks'
-          linkedin='https://linkedin.com/in/diegolima'
+          github='https://github.com/dieegoolimaa'
+          linkedin='https://www.linkedin.com/in/diego-fernandes-20/?trk=contact-info'
           imageUrl={diego}
         />
 
         <TeamMember
           name='Kivanc Keskinbora'
           role='Developer'
-          github='https://github.com/kingblocks'
+          github='https://github.com/'
           linkedin='https://linkedin.com/in/kivanckeskinbora'
           imageUrl={kivanc}
         />
