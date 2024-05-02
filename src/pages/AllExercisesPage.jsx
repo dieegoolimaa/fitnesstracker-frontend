85179b96efdd355c2b106e912862c1db7f5501a6
dieegoolimaa <dieegoolimaa@gmail.com>
2024-05-02 13:46:23 +0100

    fixed error on all exercises page

--- src/pages/AllExercisesPage.jsx
+++ src/pages/AllExercisesPage.jsx
@@ -3,18 +3,19 @@ import { Link } from "react-router-dom";
 import "../styles/AllExercisesPage.css"; // Import your CSS file
 
 const AllExercisesPage = () => {
-    const [exercises, setExercises] = useState([])
-    
-    const fetchExercises = async () => {
-        try {
-            const response = await fetch(`${import.meta.env.VITE_API_URL}/api/exercises`)
-            if (response.ok) {
-                const exercisesData = await response.json()
-                setExercises(exercisesData)
-            }
-        } catch (error) {
-            console.log(error)
-        }
+  const [exercises, setExercises] = useState([]);
+
+  const fetchExercises = async () => {
+    try {
+      const response = await fetch(
+        `${import.meta.env.VITE_API_URL}/api/exercises`
+      );
+      if (response.ok) {
+        const exercisesData = await response.json();
+        setExercises(exercisesData);
+      }
+    } catch (error) {
+      console.log(error);
     }
   };
 
@@ -22,19 +23,21 @@ const AllExercisesPage = () => {
     fetchExercises();
   }, []);
 
-    return ( 
-        <div className="exercise-container">
-            <h1>All Exercises</h1>
-            {exercises.map(currentExercise => (
-                <Link to={`/exercises/${currentExercise._id}`} key={currentExercise._id}>
-                    <div className="exercise-box">
-                        <h2>{currentExercise.name}</h2>
-                    </div>
-                </Link>
-            ))}
-        </div>
-    );
-} // commit testing 
- 
-export default AllExercisesPage;
+  return (
+    <div className="exercise-container">
+      <h1>All Exercises</h1>
+      {exercises.map((currentExercise) => (
+        <Link
+          to={`/exercises/${currentExercise._id}`}
+          key={currentExercise._id}
+        >
+          <div className="exercise-box">
+            <h2>{currentExercise.name}</h2>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
+}; // commit testing
 
+export default AllExercisesPage;
