f2a5f4386b6772d0c66e864b3cf368b906c98a16
dieegoolimaa <dieegoolimaa@gmail.com>
2024-05-06 11:55:28 +0100

    all exercises page css

--- src/pages/AllExercisesPage.jsx
+++ src/pages/AllExercisesPage.jsx
@@ -4,6 +4,8 @@ import "../styles/AllExercisesPage.css";
 const AllExercisesPage = () => {
   const [exercises, setExercises] = useState([]);
   const [selectedId, setSelectedId] = useState(null);
+  const [isExpanded, setIsExpanded] = useState({});
+
   const fetchExercises = async () => {
     try {
       const response = await fetch(
@@ -17,16 +19,42 @@ const AllExercisesPage = () => {
       console.log(error);
     }
   };
+
   useEffect(() => {
     fetchExercises();
   }, []);
+
+  const handleExpandClick = (id) => {
+    setIsExpanded({
+      ...Object.fromEntries(
+        exercises.map((exercise) => [exercise._id, exercise._id === id])
+      ),
+    });
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      const exerciseList = document.querySelector(".exercise-list");
+      if (!exerciseList.contains(event.target)) {
+        setSelectedId(null);
+        setIsExpanded({}); // Reset expanded state
+      }
+    };
+
+    document.addEventListener("click", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+
   return (
     <div className="exercise-page">
       <div className="exercise-title">
         <h1>All Exercises</h1>
       </div>
 
-      <div>
+      <div className="exercise-list">
         {exercises.map((currentExercise) => (
           <div
             key={currentExercise._id}
@@ -40,15 +68,37 @@ const AllExercisesPage = () => {
             {selectedId === currentExercise._id && (
               <>
                 <p>{currentExercise.description}</p>
-                <div>
-                  <img
-                    src={currentExercise.image1}
-                    alt={currentExercise.name}
-                  />
-                  <img
-                    src={currentExercise.image1}
-                    alt={currentExercise.name}
-                  />
+
+                <div className="setsandReps">
+                  <h4>Sets</h4>
+                  <div>
+                    <p>
+                      {currentExercise.setsandreps.sets} x
+                      {currentExercise.setsandreps.reps}
+                    </p>
+                  </div>
+                </div>
+                <div className="images">
+                  {isExpanded[currentExercise._id] && ( // Check if expanded
+                    <div>
+                      <img
+                        src={currentExercise.image1}
+                        alt={currentExercise.name}
+                      />
+                      <img
+                        src={currentExercise.image1}
+                        alt={currentExercise.name}
+                      />
+                    </div>
+                  )}
+                  <button
+                    className="expand-button"
+                    onClick={() => handleExpandClick(currentExercise._id)}
+                  >
+                    {isExpanded[currentExercise._id]
+                      ? "Hide exercise"
+                      : "See exercise"}
+                  </button>
                 </div>
               </>
             )}
@@ -58,4 +108,5 @@ const AllExercisesPage = () => {
     </div>
   );
 };
+
 export default AllExercisesPage;
